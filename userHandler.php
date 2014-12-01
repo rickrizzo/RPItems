466616ffed0b3dfab0ccd8c769875b79c3e5489c
Rob Russo <rrusso44@gmail.com>
2014-12-01 02:41:20 -0500

    New Font and Nav Bar

--- userHandler.php
+++ userHandler.php
@@ -1,14 +1,9 @@
 <?php
 	//Item Information
-	$catagory = $_POST["catagory"];
-	$item = $_POST["itemName"];
-	$price = $_POST["price"];
-	$description = $_POST["description"];
-	$img = $_POST["itemPicture"];
-	
-	//Contact Information
-	$phone = $_POST["phone"];
-	$email = $_POST["email"];
+	$fname = $_POST["firstName"];
+	$lname = $_POST["lastName"];
+	$email = $_POST["username"];
+	$password = $_POST["password"];
 	
 	/*DO NOT FORGET TO PUT THIS ON YOUR LOCAL HOST AND ADD YOUR INFORMATION*/
 	$link = mysqli_connect("localhost", "root", "mysql44") or die("ERROR: " . mysqli_error($link));
@@ -16,40 +11,36 @@
 	//Create DB
 	$dbQuery = "CREATE DATABASE IF NOT EXISTS rpItems";
 	$db = $link->query($dbQuery) or die ("DB ERROR: " . mysqli_error($link));
-	//$db->close();
 	
 	//Create Items Table
 	$link->select_db("rpItems") or die(mysqli_error());
-	$link->query("CREATE TABLE IF NOT EXISTS items (
+	$link->query("CREATE TABLE IF NOT EXISTS members (
 		id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-		itemName VARCHAR(30) NOT NULL,
-		catagory VARCHAR(30) NOT NULL,
-		price FLOAT(6, 2) NOT NULL,
-		description VARCHAR(400),
-		img VARCHAR(200),
+		firstName VARCHAR(30) NOT NULL,
+		lastName VARCHAR(30) NOT NULL,
+		email VARCHAR(40) NOT NULL,
+		password VARCHAR(30) NOT NULL,
 		PRIMARY KEY(id)
 	)") or die("TABLE ERROR: " . mysqli_error($link));
 	
 	//Insert Information
-	$strSQL = "INSERT INTO items(itemName, catagory, price, description, img)";
-	$strSQL .= "VALUES('" . $item . "', '" . $catagory . "', '" . $price . "', '";
-	$strSQL .= $description . "', '" . $img . "')";
+	$strSQL = "INSERT INTO members(firstName, lastName, email, password)";
+	$strSQL .= "VALUES('" . $fname . "', '" . $lname . "', '" . $email . "', '";
+	$strSQL .= $password . "')";
 	$insert = mysqli_query($link, $strSQL) or die ("INSERT ERROR: " . mysqli_error($link));
 	
-	$link->close();
-  
+	$link->close();  
 ?>
 
 <html>
 <head>
-	<title>Listing Posted</title>
+	<title>User Created</title>
 	<link rel="stylesheet" type="text/css" href="resources/stylesheet.css" />
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 </head>
 <body>
 	<h1>Thank You!</h1>
 	<div id="thank">
-		<h2>Your item has been uploaded</h2>
+		<h2>You are now a registered user!</h2>
 		<button><a href="index.php">Go back</a></button>
 	</div>
 </body>
