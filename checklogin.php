3a22ee98675c74a5a7cb2f8ca43c34ce3d283d60
Kayla Cinnamon <cinnak@rpi.edu>
2014-12-04 13:08:29 -0500

    Fixed indentation

--- checklogin.php
+++ checklogin.php
@@ -1,6 +1,6 @@
 <?php  
-session_start();  
-if($_SESSION[username]==""){  
+  session_start();  
+  if($_SESSION[username]==""){  
     echo "<script>alert('Please Login!');window.location.href='login.html';</script>";  
   }  
 ?> 
