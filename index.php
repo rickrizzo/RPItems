764a48d246e7f8a0a8e93da4beeca4d5fb585ea2
Kayla Cinnamon <cinnak@rpi.edu>
2014-12-04 14:10:41 -0500

    Fixed indentation

--- index.php
+++ index.php
@@ -28,69 +28,68 @@
         <label for="search">Find what you need<br /></label>
         <input type="search" id="search" name="search"/>
         <button type="submit" id="searchSubmit">Search</button>
-		</form>
-		
-		<br/>
-		
-		<form method="post" action="categorySearchHandler.php">
-			<label for="category_select">Or browse by category</label>
-			<br/>
-			<select name="category_select" >
-			  <option value="">Choose...</option>
-              <option value="Books">Books</option>
-              <option value="Clothing">Clothing</option>
-              <option value="Electronics">Electronics</option>
-              <option value="Games">Games</option>
-              <option value="Movies">Movies</option>
-			  <option value="Music">Music</option>
-              <option value="Tools">Tool</option>
-              <option value="Other">Others</option>
-			</select>
-			<input type="submit" value="Search"/>
-		</form>
-		<br/>
-		
-		<!--List New Item-->
-		<button onclick="window.location.href='newlisting.html'">List New Item</button>
-		
-		<!--Popular Items-->
-		<div id="popular">
-			<h3>Popular Items</h3>
-			<table>
-				<?php 
-					$db = mysqli_connect('localhost', 'root', '', 'rpitems') or die(mysqli_error($db));
-					$query = 'select * from items';
-					$result = $db->query($query);
-					$numItems = $result->num_rows;
-					
-					for($i = 0; $i < $numItems; $i ++){
-						$record = $result->fetch_assoc();
-						if($i % 5 == 0){
-							if($i != 0){
-								echo "</tr>";
-							}
-							echo "<tr>";
-							
-						}
-						echo "<td><a href='productpage.php?item=" . $record['itemName'] . "'>
-							<h5>" . $record['itemName']."</h5>
-							<p>$" . $record['price'] ."</p>
-							<img src = '". $record['img'] ."' height='200' width = '200' >
-							</a></td>";
-							
-						if($i == 8){
-							break;
-						}
-					}
-					
-					$result->free();
-					$db->close();
-				?>
-			</table>
-		</div>
-	</div>
-	<footer>
-		<p>Created Winter 2014 for Intro to Information Technology and Web Science</p>
-	</footer>
-</body>
+      </form>
+      
+      <br/>
+      
+      <form method="post" action="categorySearchHandler.php">
+        <label for="category_select">Or browse by category</label>
+        <br/>
+        <select name="category_select" >
+          <option value="">Choose...</option>
+          <option value="Books">Books</option>
+          <option value="Clothing">Clothing</option>
+          <option value="Electronics">Electronics</option>
+          <option value="Games">Games</option>
+          <option value="Movies">Movies</option>
+          <option value="Music">Music</option>
+          <option value="Tools">Tool</option>
+          <option value="Other">Others</option>
+        </select>
+        <input type="submit" value="Search"/>
+      </form>
+      <br/>
+      
+      <!--List New Item-->
+      <button onclick="window.location.href='newlisting.html'">List New Item</button>
+      
+      <!--Popular Items-->
+      <div id="popular">
+        <h3>Popular Items</h3>
+        <table>
+          <?php
+            $db = mysqli_connect('localhost', 'root', '', 'rpitems') or die(mysqli_error($db));
+            $query = 'select * from items';
+            $result = $db->query($query);
+            $numItems = $result->num_rows;
+            
+            for($i = 0; $i < $numItems; $i ++){
+              $record = $result->fetch_assoc();
+              if($i % 5 == 0){
+                if($i != 0){
+                  echo "</tr>";
+                }
+                echo "<tr>";
+              }
+              echo "<td><a href='productpage.php?item=" . $record['itemName'] . "'>
+              <h5>" . $record['itemName']."</h5>
+              <p>$" . $record['price'] ."</p>
+              <img src = '". $record['img'] ."' height='200' width = '200' >
+              </a></td>";
+                
+              if($i == 8){
+                break;
+              }
+            }
+            
+            $result->free();
+            $db->close();
+          ?>
+        </table>
+      </div>
+    </div>
+    <footer>
+      <p>Created Winter 2014 for Intro to Information Technology and Web Science</p>
+    </footer>
+  </body>
 </html>
